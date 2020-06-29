41cbd85c9739afcf998b4f1f92813556e423d1d7
javier salas <javier.salas.991@gmail.com>
2020-06-29 12:02:15 -0300

    se corrigio el problema segundos. falta maquetado

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,6 +1,7 @@
 let minutos = document.getElementById('idMinutos');
 let segundos = document.getElementById('idSegundos');
 let centesimas = document.getElementById('idCentesimas');
+let estado = 'pausado';
 
 
 function fcentesimas() {
@@ -16,63 +17,98 @@ function fcentesimas() {
         }
         else {
             centesimas.innerText = '00'
-        }
-    }
-}
-function fsegundos() {
-    let seg = 00
-    if (parseInt(segundos.innerText) < 9) {
-        seg = parseInt(segundos.innerText) + 1
-        segundos.innerText = '0' + seg;
-    } else {
-        if (segundos.innerText < 59) {
-            seg = parseInt(segundos.innerText) + 1
-            segundos.innerText = seg;
+            let seg = 00
+            if (parseInt(segundos.innerText) < 9) {
+                seg = parseInt(segundos.innerText) + 1
+                segundos.innerText = '0' + seg;
+            } else {
+                if (segundos.innerText < 59) {
+                    seg = parseInt(segundos.innerText) + 1
+                    segundos.innerText = seg;
 
-        }
-        else {
-            segundos.innerText = '00'
-        }
-    }
-}
-function fminutos() {
-    let min = 00
-    if (parseInt(minutos.innerText) < 9) {
-        min = parseInt(minutos.innerText) + 1
-        minutos.innerText = '0' + min;
-    } else {
-        if (minutos.innerText < 59) {
-            min = parseInt(min.innerText) + 1
-            min.innerText = min;
+                }
+                else {
+                    segundos.innerText = '00'
+                    let min = 00
+                    if (parseInt(minutos.innerText) < 9) {
+                        min = parseInt(minutos.innerText) + 1
+                        minutos.innerText = '0' + min;
+                    } else {
+                        if (minutos.innerText < 59) {
+                            min = parseInt(min.innerText) + 1
+                            min.innerText = min;
 
-        }
-        else {
-            minutos.innerText = '00'
+                        }
+                        else {
+                            minutos.innerText = '00'
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
+// function fsegundos() {
+//     let seg = 00
+//     if (parseInt(segundos.innerText) < 9) {
+//         seg = parseInt(segundos.innerText) + 1
+//         segundos.innerText = '0' + seg;
+//     } else {
+//         if (segundos.innerText < 59) {
+//             seg = parseInt(segundos.innerText) + 1
+//             segundos.innerText = seg;
+
+//         }
+//         else {
+//             segundos.innerText = '00'
+//         }
+//     }
+// }
+// function fminutos() {
+//     let min = 00
+//     if (parseInt(minutos.innerText) < 9) {
+//         min = parseInt(minutos.innerText) + 1
+//         minutos.innerText = '0' + min;
+//     } else {
+//         if (minutos.innerText < 59) {
+//             min = parseInt(min.innerText) + 1
+//             min.innerText = min;
+
+//         }
+//         else {
+//             minutos.innerText = '00'
+//         }
+//     }
+// }
 let t1
 let t2
 let t3
 
 function arranque() {
-    t1 = window.setInterval(fcentesimas, 10)
-    t2 = window.setInterval(fsegundos, 1000)
-    t3 = window.setInterval(fminutos, 60000)
+    if (estado == 'pausado') {
+        estado = 'andando'
+        t1 = window.setInterval(fcentesimas, 10)
+        t2 = window.setInterval(fsegundos, 1000)
+        t3 = window.setInterval(fminutos, 60000)
+    }
+
 }
 
 function pausa() {
+    estado = 'pausado'
     window.clearInterval(t1)
     window.clearInterval(t2)
     window.clearInterval(t3)
 }
 
-function stop(){
+function stop() {
+    estado = 'pausado'
     window.clearInterval(t1)
     window.clearInterval(t2)
     window.clearInterval(t3)
     minutos.innerText = '00'
-    segundos.innerText= '00'
-    centesimas.innerText= '00'
+    segundos.innerText = '00'
+    centesimas.innerText = '00'
 }
 
