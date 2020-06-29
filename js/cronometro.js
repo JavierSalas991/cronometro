f2da5d636ecfef003defc1dd424cfaba83900e12
javier salas <javier.salas.991@gmail.com>
2020-06-29 15:40:43 -0300

    se unio pausa y play y se agrego font AWESOME

--- js/cronometro.js
+++ js/cronometro.js
@@ -49,64 +49,33 @@ function fcentesimas() {
         }
     }
 }
-// function fsegundos() {
-//     let seg = 00
-//     if (parseInt(segundos.innerText) < 9) {
-//         seg = parseInt(segundos.innerText) + 1
-//         segundos.innerText = '0' + seg;
-//     } else {
-//         if (segundos.innerText < 59) {
-//             seg = parseInt(segundos.innerText) + 1
-//             segundos.innerText = seg;
 
-//         }
-//         else {
-//             segundos.innerText = '00'
-//         }
-//     }
-// }
-// function fminutos() {
-//     let min = 00
-//     if (parseInt(minutos.innerText) < 9) {
-//         min = parseInt(minutos.innerText) + 1
-//         minutos.innerText = '0' + min;
-//     } else {
-//         if (minutos.innerText < 59) {
-//             min = parseInt(min.innerText) + 1
-//             min.innerText = min;
-
-//         }
-//         else {
-//             minutos.innerText = '00'
-//         }
-//     }
-// }
 let t1
-let t2
-let t3
 
 function arranque() {
     if (estado == 'pausado') {
         estado = 'andando'
         t1 = window.setInterval(fcentesimas, 10)
-        t2 = window.setInterval(fsegundos, 1000)
-        t3 = window.setInterval(fminutos, 60000)
+
+        let playPause = document.getElementById('playPause')
+        playPause.innerHTML = '<i class="fas fa-pause"></i>';
+    }
+    else {
+        estado = 'pausado'
+        window.clearInterval(t1)
+        let playPause = document.getElementById('playPause')
+        playPause.innerHTML = ' <i class="fas fa-play"></i>';
+
     }
 
 }
 
-function pausa() {
-    estado = 'pausado'
-    window.clearInterval(t1)
-    window.clearInterval(t2)
-    window.clearInterval(t3)
-}
 
 function stop() {
     estado = 'pausado'
     window.clearInterval(t1)
-    window.clearInterval(t2)
-    window.clearInterval(t3)
+    let playPause = document.getElementById('playPause')
+    playPause.innerHTML = ' <i class="fas fa-play"></i>';
     minutos.innerText = '00'
     segundos.innerText = '00'
     centesimas.innerText = '00'
